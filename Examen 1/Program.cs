d1facd8b0ee716c40c2e6e5f923a60a01dcb75d9
mathiashron <mathiashron@gmail.com>
2016-10-10 22:39:52 -0600

    Completado Problema 1 y 2

---
+++ Examen 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_1
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+
+            DiagonalDifference diagonal = new DiagonalDifference();
+            string matriz = "11 2 4 5 | 4 5 6 8 | 10 8 -12 2 | 7 4 8 0";
+            int val = diagonal.matriz(matriz);
+            Console.WriteLine("El resultado de DiagonalDifference es: {0}", val);
+            //Console.Read();
+
+            camelCase camel = new camelCase();
+            string dato = "saveChangesInTheEditor";
+            int val1 = camel.calculate(dato);
+            Console.WriteLine("La cantidad de palabras camelCase : {0}", val1);
+            Console.Read();
+
+
+
+
+        }
+    }
+}
---
+++ Examen 1/camelCase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Examen_1
+{
+    public class camelCase
+    {
+        public int calculate(string cal)
+        {
+            ///<summary>
+            ///Lo primero que realizo es separar las fraces que estan en mayusculas
+            ///eso le agrega un espacio luego con el split cuentos los espacios y eso me da la cantidad
+            ///de palabras
+            ///</summary>
+            int dato = 0;
+            var separar = Regex.Replace(cal, "(((?<!^)[A-Z](?=[a-z]))|((?<=[a-z])[A-Z]))", " $1");
+            //Console.WriteLine(output);
+            string[] lines = separar.Split(' ');
+            foreach (string line in lines)
+            {
+                dato++;
+
+            }
+
+            return dato;
+        }
+
+    }
+}
